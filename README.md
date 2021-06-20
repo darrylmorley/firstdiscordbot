3e69667bc79fec6d463ccb08c7048875ae0939c9
Darryl Morley <darrylmorley.uk@gmail.com>
2021-06-20 22:08:36 +0100

    add hacker news

---
+++ README.md
@@ -0,0 +1,7 @@
+# A Simple Discord Bot
+
+My first attempt at creating a discord bot. This is a simple bot that currently reponds to the following:
+
+!hi: Says Hi!
+!dadjoke: returns a dad joke from the dadjoke api
+!hn: returns a random hacker news aritcle from the top hacker news articles api
--- commands.js
+++ commands.js
@@ -1,25 +1,26 @@
-const axios = require("axios");
-
-const getDadJoke = async () => {
-  const getJoke = await axios.get("https://icanhazdadjoke.com/", {
-    headers: {
-      Accept: "application/json",
-    },
-  });
-
-  return getJoke.data.joke;
-};
+const getDadJoke = require("./lib/getDadJoke");
+const getHackerNews = require("./lib/getHackerNewsStory");
 
 async function gotMessage(msg) {
-  if (msg.channel == "856220709520146450" && msg.content === "!hi") {
-    msg.reply("Hi");
+  const responses = {
+    "!hi": "Hi!!",
+    "!dadjoke": await getDadJoke(),
+    "!hn": await getHackerNews(),
+  };
+
+  if (responses[msg.content]) {
+    msg.channel.send(responses[msg.content]);
   }
 
-  if (msg.channel == "856220709520146450" && msg.content === "!dadjoke") {
-    const dadJoke = await getDadJoke();
-    console.log(dadJoke);
-    msg.reply(`${dadJoke}`);
-  }
+  // if (msg.channel == "856220709520146450" && msg.content === "!hi") {
+  //   msg.reply("Hi");
+  // }
+
+  // if (msg.channel == "856220709520146450" && msg.content === "!dadjoke") {
+  //   const dadJoke = await getDadJoke();
+  //   console.log(dadJoke);
+  //   msg.reply(`${dadJoke}`);
+  // }
 }
 
 module.exports = gotMessage;
